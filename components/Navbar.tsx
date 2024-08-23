50a52c2cdcc1d432a7627dc9129098770ff8fc8a
mlaskowski7 <mtlaskowski7@gmail.com>
2024-08-23 20:35:08 +0200

    created navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,35 @@
+import { NavLink, navLinks } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
+import { title } from "process";
 import React from "react";
 
 const Navbar = () => {
   return (
     <nav className="flex-between fixed z-50 w-full gap-5 p-6 shadow-sm dark:shadow-none sm:px-12">
-      <Link href="/" className="flex items-center gap-2">
-        <Image src="/rust.png" alt="rustacean" width={20} height={20} />
-        <p className="font-bold font-roboto max-sm:hidden">
-          M
-          <span className="text-tertiary dark:text-dark-tertiary">
-            LASKOWSKI7
-          </span>
-        </p>
-      </Link>
-
-      <div>
-        <ul>
-          <li></li>
-        </ul>
+      <div className="flex gap-10">
+        <Link href="/" className="flex items-center gap-3">
+          <Image src="/logo.png" alt="rustacean" width={40} height={40} />
+          <p className="font-bold font-roboto max-sm:hidden">
+            M
+            <span className="text-tertiary dark:text-dark-tertiary">
+              LASKOWSKI7
+            </span>
+          </p>
+        </Link>
+        <div className="flex items-center gap-2 text-tertiary dark:text-dark-tertiary font-spaceGrotesk">
+          {navLinks.map(
+            (item: NavLink, index: number): JSX.Element => (
+              <Link
+                key={index}
+                className="text-center hover:text-secondary dark:hover:text-white ease-in-out duration-300"
+                href={`/${item.link}`}
+              >
+                {item.title}
+              </Link>
+            )
+          )}
+        </div>
       </div>
     </nav>
   );
---
+++ lib/constants.ts
@@ -0,0 +1,23 @@
+export interface NavLink {
+  title: String;
+  link: String;
+}
+
+export const navLinks: NavLink[] = [
+  {
+    title: "ABOUT",
+    link: "#about",
+  },
+  {
+    title: "TECHNOLOGIES",
+    link: "#technologies",
+  },
+  {
+    title: "EXPERIENCE/EDUCATION",
+    link: "#experience",
+  },
+  {
+    title: "PROJECTS",
+    link: "#projects",
+  },
+];
